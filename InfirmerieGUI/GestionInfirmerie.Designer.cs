59156545667715b009d0dad098b2196c20b0c14d
Raphaël TOURSEL <thewhiteshadowgaming@gmail.com>
2023-11-08 10:46:17 +0100

    Creation du formulaire de connexion, et son style

--- InfirmerieGUI/GestionInfirmerie.Designer.cs
+++ InfirmerieGUI/GestionInfirmerie.Designer.cs
@@ -29,21 +29,105 @@ namespace InfirmerieGUI
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GestionInfirmerie));
+            this.pageTitle = new System.Windows.Forms.Label();
+            this.labelId = new System.Windows.Forms.Label();
+            this.loginId = new System.Windows.Forms.TextBox();
+            this.loginPassword = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.connectButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // pageTitle
+            // 
+            this.pageTitle.AutoSize = true;
+            this.pageTitle.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pageTitle.Location = new System.Drawing.Point(488, 138);
+            this.pageTitle.Name = "pageTitle";
+            this.pageTitle.Size = new System.Drawing.Size(139, 36);
+            this.pageTitle.TabIndex = 0;
+            this.pageTitle.Text = "Connexion";
+            this.pageTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelId
+            // 
+            this.labelId.AutoSize = true;
+            this.labelId.Font = new System.Drawing.Font("Calibri Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelId.Location = new System.Drawing.Point(369, 233);
+            this.labelId.Name = "labelId";
+            this.labelId.Size = new System.Drawing.Size(98, 23);
+            this.labelId.TabIndex = 1;
+            this.labelId.Text = "Identifiant :";
+            // 
+            // loginId
+            // 
+            this.loginId.Location = new System.Drawing.Point(473, 237);
+            this.loginId.Name = "loginId";
+            this.loginId.Size = new System.Drawing.Size(171, 21);
+            this.loginId.TabIndex = 2;
+            // 
+            // loginPassword
+            // 
+            this.loginPassword.Location = new System.Drawing.Point(473, 284);
+            this.loginPassword.Name = "loginPassword";
+            this.loginPassword.Size = new System.Drawing.Size(171, 21);
+            this.loginPassword.TabIndex = 4;
+            this.loginPassword.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Font = new System.Drawing.Font("Calibri Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPassword.Location = new System.Drawing.Point(347, 280);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(120, 23);
+            this.labelPassword.TabIndex = 3;
+            this.labelPassword.Text = "Mot de Passe :";
+            this.labelPassword.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // connectButton
+            // 
+            this.connectButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.connectButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.connectButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.connectButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.connectButton.Font = new System.Drawing.Font("Calibri Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connectButton.Location = new System.Drawing.Point(494, 356);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(128, 33);
+            this.connectButton.TabIndex = 5;
+            this.connectButton.Text = "Connexion";
+            this.connectButton.UseVisualStyleBackColor = false;
+            // 
+            // GestionInfirmerie
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1180, 529);
-            this.Name = "Form1";
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.loginPassword);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.loginId);
+            this.Controls.Add(this.labelId);
+            this.Controls.Add(this.pageTitle);
+            this.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GestionInfirmerie";
             this.Text = "GestionInfirmerie";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label pageTitle;
+        private System.Windows.Forms.Label labelId;
+        private System.Windows.Forms.TextBox loginId;
+        private System.Windows.Forms.TextBox loginPassword;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Button connectButton;
     }
 }
 
--- InfirmerieGUI/GestionInfirmerie.cs
+++ InfirmerieGUI/GestionInfirmerie.cs
@@ -21,5 +21,20 @@ namespace InfirmerieGUI
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
