a51ec9f186598b2948a0b066745d78ea8f46622d
Lucas Dupas <dupaslucas8@gmail.com>
2023-11-09 08:51:20 +0100

    Création connexion BDD

---
+++ InfirmerieDAL/Database.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace InfirmerieDAL
+{
+    public class Database
+    {
+        static bool Connect()
+        {
+            SqlConnection DbConnect = new SqlConnection("Persist Security Info=False; Trusted_Connection=True; database=GestionInfirmerie; server=(local)");
+            try
+            {
+                DbConnect.Open();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return false;
+            }
+        }
+
+    }
+}
