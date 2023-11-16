69becffeebb625c0c6512db7dc34cd64eb1fe535
Sallak Imrane <imraneeslk@gmail.com>
2023-11-16 15:36:03 +0100

    Style Eleve GUI

--- InfirmerieGUI/GestionEleve.Designer.cs
+++ InfirmerieGUI/GestionEleve.Designer.cs
@@ -46,11 +46,12 @@ namespace InfirmerieGUI
             // 
             // lblAccueil
             // 
+            this.lblAccueil.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.lblAccueil.AutoSize = true;
-            this.lblAccueil.Font = new System.Drawing.Font("Agency FB", 14F);
+            this.lblAccueil.Font = new System.Drawing.Font("Calibri", 20F, System.Drawing.FontStyle.Bold);
             this.lblAccueil.Location = new System.Drawing.Point(658, 33);
             this.lblAccueil.Name = "lblAccueil";
-            this.lblAccueil.Size = new System.Drawing.Size(59, 28);
+            this.lblAccueil.Size = new System.Drawing.Size(107, 41);
             this.lblAccueil.TabIndex = 0;
             this.lblAccueil.Text = "Élèves";
             this.lblAccueil.Click += new System.EventHandler(this.lblAccueil_Click);
@@ -58,6 +59,10 @@ namespace InfirmerieGUI
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(255, 88);
@@ -71,8 +76,10 @@ namespace InfirmerieGUI
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.BackColor = System.Drawing.Color.IndianRed;
             this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Font = new System.Drawing.Font("Calibri", 9F);
             this.button1.Location = new System.Drawing.Point(892, 37);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(88, 30);
@@ -82,7 +89,9 @@ namespace InfirmerieGUI
             // 
             // button2
             // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button2.BackColor = System.Drawing.Color.LemonChiffon;
+            this.button2.Font = new System.Drawing.Font("Calibri", 9F);
             this.button2.Location = new System.Drawing.Point(986, 37);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(101, 30);
@@ -93,7 +102,9 @@ namespace InfirmerieGUI
             // 
             // button3
             // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button3.BackColor = System.Drawing.Color.LightGreen;
+            this.button3.Font = new System.Drawing.Font("Calibri", 9F);
             this.button3.Location = new System.Drawing.Point(1093, 37);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 30);
@@ -103,50 +114,52 @@ namespace InfirmerieGUI
             // 
             // menuStrip1
             // 
-            this.menuStrip1.AutoSize = false;
             this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuStrip1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.visitesToolStripMenuItem,
-            this.médicamentsToolStripMenuItem,
             this.élèvesToolStripMenuItem,
+            this.médicamentsToolStripMenuItem,
             this.statistiquesToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
-            this.menuStrip1.Size = new System.Drawing.Size(200, 529);
+            this.menuStrip1.Size = new System.Drawing.Size(165, 529);
             this.menuStrip1.TabIndex = 5;
             this.menuStrip1.Text = "menuStrip1";
             this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
             // 
             // visitesToolStripMenuItem
             // 
-            this.visitesToolStripMenuItem.Font = new System.Drawing.Font("Microsoft JhengHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.visitesToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.visitesToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("visitesToolStripMenuItem.Image")));
             this.visitesToolStripMenuItem.Name = "visitesToolStripMenuItem";
-            this.visitesToolStripMenuItem.Size = new System.Drawing.Size(193, 24);
+            this.visitesToolStripMenuItem.Size = new System.Drawing.Size(150, 28);
             this.visitesToolStripMenuItem.Text = "Visites";
             // 
             // médicamentsToolStripMenuItem
             // 
-            this.médicamentsToolStripMenuItem.Font = new System.Drawing.Font("Microsoft JhengHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.médicamentsToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.médicamentsToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("médicamentsToolStripMenuItem.Image")));
             this.médicamentsToolStripMenuItem.Name = "médicamentsToolStripMenuItem";
-            this.médicamentsToolStripMenuItem.Size = new System.Drawing.Size(193, 24);
+            this.médicamentsToolStripMenuItem.Size = new System.Drawing.Size(150, 28);
             this.médicamentsToolStripMenuItem.Text = "Médicaments";
             // 
             // élèvesToolStripMenuItem
             // 
+            this.élèvesToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.élèvesToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("élèvesToolStripMenuItem.Image")));
             this.élèvesToolStripMenuItem.Name = "élèvesToolStripMenuItem";
-            this.élèvesToolStripMenuItem.Size = new System.Drawing.Size(193, 24);
+            this.élèvesToolStripMenuItem.Size = new System.Drawing.Size(150, 28);
             this.élèvesToolStripMenuItem.Text = "Élèves";
             // 
             // statistiquesToolStripMenuItem
             // 
+            this.statistiquesToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.statistiquesToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("statistiquesToolStripMenuItem.Image")));
             this.statistiquesToolStripMenuItem.Name = "statistiquesToolStripMenuItem";
-            this.statistiquesToolStripMenuItem.Size = new System.Drawing.Size(193, 24);
+            this.statistiquesToolStripMenuItem.Size = new System.Drawing.Size(150, 28);
             this.statistiquesToolStripMenuItem.Text = "Statistiques";
             // 
             // GestionEleve
