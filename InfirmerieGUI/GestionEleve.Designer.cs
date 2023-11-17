1491850ad180650086ce514dda7139172a446afc
Lucas DUPAS <dupaslucas8@gmail.com>
2023-11-17 10:29:45 +0100

    Modification d'un élève

--- InfirmerieGUI/GestionEleve.Designer.cs
+++ InfirmerieGUI/GestionEleve.Designer.cs
@@ -40,8 +40,24 @@ namespace InfirmerieGUI
             this.élèvesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.médicamentsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.statistiquesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlModif = new System.Windows.Forms.Panel();
+            this.lblPRenomModif = new System.Windows.Forms.Label();
+            this.txtPrenomModif = new System.Windows.Forms.TextBox();
+            this.txtIdModif = new System.Windows.Forms.TextBox();
+            this.lblNomModif = new System.Windows.Forms.Label();
+            this.txtNomModif = new System.Windows.Forms.TextBox();
+            this.lblBirthdate = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.lblTelEleveModif = new System.Windows.Forms.Label();
+            this.txtNumeroEleve = new System.Windows.Forms.TextBox();
+            this.lblNumeroTelParentModif = new System.Windows.Forms.Label();
+            this.txtNumeroParent = new System.Windows.Forms.TextBox();
+            this.chkExtraTime = new System.Windows.Forms.CheckBox();
+            this.txtComments = new System.Windows.Forms.TextBox();
+            this.lblComments = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.menuStrip1.SuspendLayout();
+            this.pnlModif.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblAccueil
@@ -49,7 +65,7 @@ namespace InfirmerieGUI
             this.lblAccueil.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.lblAccueil.AutoSize = true;
             this.lblAccueil.Font = new System.Drawing.Font("Calibri", 20F, System.Drawing.FontStyle.Bold);
-            this.lblAccueil.Location = new System.Drawing.Point(658, 33);
+            this.lblAccueil.Location = new System.Drawing.Point(932, 33);
             this.lblAccueil.Name = "lblAccueil";
             this.lblAccueil.Size = new System.Drawing.Size(107, 41);
             this.lblAccueil.TabIndex = 0;
@@ -59,17 +75,15 @@ namespace InfirmerieGUI
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToOrderColumns = true;
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(255, 88);
+            this.dataGridView1.Location = new System.Drawing.Point(296, 92);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(872, 376);
+            this.dataGridView1.Size = new System.Drawing.Size(1420, 298);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
@@ -80,7 +94,7 @@ namespace InfirmerieGUI
             this.button1.BackColor = System.Drawing.Color.IndianRed;
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.Font = new System.Drawing.Font("Calibri", 9F);
-            this.button1.Location = new System.Drawing.Point(892, 37);
+            this.button1.Location = new System.Drawing.Point(1322, 44);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(88, 30);
             this.button1.TabIndex = 2;
@@ -93,7 +107,7 @@ namespace InfirmerieGUI
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button2.BackColor = System.Drawing.Color.LemonChiffon;
             this.button2.Font = new System.Drawing.Font("Calibri", 9F);
-            this.button2.Location = new System.Drawing.Point(986, 37);
+            this.button2.Location = new System.Drawing.Point(1416, 44);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(101, 30);
             this.button2.TabIndex = 3;
@@ -106,7 +120,7 @@ namespace InfirmerieGUI
             this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button3.BackColor = System.Drawing.Color.LightGreen;
             this.button3.Font = new System.Drawing.Font("Calibri", 9F);
-            this.button3.Location = new System.Drawing.Point(1093, 37);
+            this.button3.Location = new System.Drawing.Point(1523, 44);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 30);
             this.button3.TabIndex = 4;
@@ -116,6 +130,7 @@ namespace InfirmerieGUI
             // 
             // menuStrip1
             // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Left;
             this.menuStrip1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
@@ -127,7 +142,7 @@ namespace InfirmerieGUI
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
-            this.menuStrip1.Size = new System.Drawing.Size(163, 529);
+            this.menuStrip1.Size = new System.Drawing.Size(163, 700);
             this.menuStrip1.TabIndex = 5;
             this.menuStrip1.Text = "menuStrip1";
             this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
@@ -164,11 +179,155 @@ namespace InfirmerieGUI
             this.statistiquesToolStripMenuItem.Size = new System.Drawing.Size(150, 28);
             this.statistiquesToolStripMenuItem.Text = "Statistiques";
             // 
+            // pnlModif
+            // 
+            this.pnlModif.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pnlModif.Controls.Add(this.lblComments);
+            this.pnlModif.Controls.Add(this.txtComments);
+            this.pnlModif.Controls.Add(this.chkExtraTime);
+            this.pnlModif.Controls.Add(this.lblNumeroTelParentModif);
+            this.pnlModif.Controls.Add(this.txtNumeroParent);
+            this.pnlModif.Controls.Add(this.lblTelEleveModif);
+            this.pnlModif.Controls.Add(this.txtNumeroEleve);
+            this.pnlModif.Controls.Add(this.monthCalendar1);
+            this.pnlModif.Controls.Add(this.lblBirthdate);
+            this.pnlModif.Controls.Add(this.lblPRenomModif);
+            this.pnlModif.Controls.Add(this.txtPrenomModif);
+            this.pnlModif.Controls.Add(this.txtIdModif);
+            this.pnlModif.Controls.Add(this.lblNomModif);
+            this.pnlModif.Controls.Add(this.txtNomModif);
+            this.pnlModif.Location = new System.Drawing.Point(296, 426);
+            this.pnlModif.Name = "pnlModif";
+            this.pnlModif.Size = new System.Drawing.Size(1232, 478);
+            this.pnlModif.TabIndex = 6;
+            this.pnlModif.Visible = false;
+            this.pnlModif.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // lblPRenomModif
+            // 
+            this.lblPRenomModif.AutoSize = true;
+            this.lblPRenomModif.Location = new System.Drawing.Point(3, 109);
+            this.lblPRenomModif.Name = "lblPRenomModif";
+            this.lblPRenomModif.Size = new System.Drawing.Size(57, 17);
+            this.lblPRenomModif.TabIndex = 4;
+            this.lblPRenomModif.Text = "Prenom";
+            // 
+            // txtPrenomModif
+            // 
+            this.txtPrenomModif.Location = new System.Drawing.Point(66, 109);
+            this.txtPrenomModif.Name = "txtPrenomModif";
+            this.txtPrenomModif.Size = new System.Drawing.Size(141, 22);
+            this.txtPrenomModif.TabIndex = 3;
+            // 
+            // txtIdModif
+            // 
+            this.txtIdModif.Location = new System.Drawing.Point(0, 3);
+            this.txtIdModif.Name = "txtIdModif";
+            this.txtIdModif.Size = new System.Drawing.Size(100, 22);
+            this.txtIdModif.TabIndex = 2;
+            this.txtIdModif.Visible = false;
+            // 
+            // lblNomModif
+            // 
+            this.lblNomModif.AutoSize = true;
+            this.lblNomModif.Location = new System.Drawing.Point(3, 55);
+            this.lblNomModif.Name = "lblNomModif";
+            this.lblNomModif.Size = new System.Drawing.Size(41, 17);
+            this.lblNomModif.TabIndex = 1;
+            this.lblNomModif.Text = "Nom:";
+            // 
+            // txtNomModif
+            // 
+            this.txtNomModif.Location = new System.Drawing.Point(66, 55);
+            this.txtNomModif.Name = "txtNomModif";
+            this.txtNomModif.Size = new System.Drawing.Size(141, 22);
+            this.txtNomModif.TabIndex = 0;
+            // 
+            // lblBirthdate
+            // 
+            this.lblBirthdate.AutoSize = true;
+            this.lblBirthdate.Location = new System.Drawing.Point(3, 165);
+            this.lblBirthdate.Name = "lblBirthdate";
+            this.lblBirthdate.Size = new System.Drawing.Size(130, 17);
+            this.lblBirthdate.TabIndex = 6;
+            this.lblBirthdate.Text = "Date de naissance:";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(6, 191);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 7;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // lblTelEleveModif
+            // 
+            this.lblTelEleveModif.AutoSize = true;
+            this.lblTelEleveModif.Location = new System.Drawing.Point(304, 55);
+            this.lblTelEleveModif.Name = "lblTelEleveModif";
+            this.lblTelEleveModif.Size = new System.Drawing.Size(101, 17);
+            this.lblTelEleveModif.TabIndex = 9;
+            this.lblTelEleveModif.Text = "Numero Eleve:";
+            this.lblTelEleveModif.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtNumeroEleve
+            // 
+            this.txtNumeroEleve.Location = new System.Drawing.Point(411, 52);
+            this.txtNumeroEleve.Name = "txtNumeroEleve";
+            this.txtNumeroEleve.Size = new System.Drawing.Size(141, 22);
+            this.txtNumeroEleve.TabIndex = 8;
+            this.txtNumeroEleve.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // lblNumeroTelParentModif
+            // 
+            this.lblNumeroTelParentModif.AutoSize = true;
+            this.lblNumeroTelParentModif.Location = new System.Drawing.Point(258, 112);
+            this.lblNumeroTelParentModif.Name = "lblNumeroTelParentModif";
+            this.lblNumeroTelParentModif.Size = new System.Drawing.Size(147, 17);
+            this.lblNumeroTelParentModif.TabIndex = 11;
+            this.lblNumeroTelParentModif.Text = "Numero Parent Eleve:";
+            // 
+            // txtNumeroParent
+            // 
+            this.txtNumeroParent.Location = new System.Drawing.Point(411, 109);
+            this.txtNumeroParent.Name = "txtNumeroParent";
+            this.txtNumeroParent.Size = new System.Drawing.Size(141, 22);
+            this.txtNumeroParent.TabIndex = 10;
+            // 
+            // chkExtraTime
+            // 
+            this.chkExtraTime.AutoSize = true;
+            this.chkExtraTime.Location = new System.Drawing.Point(631, 42);
+            this.chkExtraTime.Name = "chkExtraTime";
+            this.chkExtraTime.Size = new System.Drawing.Size(103, 21);
+            this.chkExtraTime.TabIndex = 12;
+            this.chkExtraTime.Text = "Tier-Temps";
+            this.chkExtraTime.UseVisualStyleBackColor = true;
+            this.chkExtraTime.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // txtComments
+            // 
+            this.txtComments.Location = new System.Drawing.Point(740, 112);
+            this.txtComments.MinimumSize = new System.Drawing.Size(0, 50);
+            this.txtComments.Name = "txtComments";
+            this.txtComments.Size = new System.Drawing.Size(150, 22);
+            this.txtComments.TabIndex = 13;
+            this.txtComments.TextChanged += new System.EventHandler(this.txtComments_TextChanged);
+            // 
+            // lblComments
+            // 
+            this.lblComments.AutoSize = true;
+            this.lblComments.Location = new System.Drawing.Point(591, 112);
+            this.lblComments.Name = "lblComments";
+            this.lblComments.Size = new System.Drawing.Size(143, 17);
+            this.lblComments.TabIndex = 14;
+            this.lblComments.Text = "Commentaires Santé:";
+            // 
             // GestionEleve
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1180, 529);
+            this.ClientSize = new System.Drawing.Size(1728, 700);
+            this.Controls.Add(this.pnlModif);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -183,6 +342,8 @@ namespace InfirmerieGUI
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.pnlModif.ResumeLayout(false);
+            this.pnlModif.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -201,5 +362,20 @@ namespace InfirmerieGUI
         private System.Windows.Forms.ToolStripMenuItem médicamentsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem statistiquesToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem élèvesToolStripMenuItem;
+        private System.Windows.Forms.Panel pnlModif;
+        private System.Windows.Forms.Label lblNomModif;
+        private System.Windows.Forms.TextBox txtNomModif;
+        private System.Windows.Forms.TextBox txtIdModif;
+        private System.Windows.Forms.Label lblPRenomModif;
+        private System.Windows.Forms.TextBox txtPrenomModif;
+        private System.Windows.Forms.Label lblBirthdate;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label lblTelEleveModif;
+        private System.Windows.Forms.TextBox txtNumeroEleve;
+        private System.Windows.Forms.Label lblNumeroTelParentModif;
+        private System.Windows.Forms.TextBox txtNumeroParent;
+        private System.Windows.Forms.CheckBox chkExtraTime;
+        private System.Windows.Forms.Label lblComments;
+        private System.Windows.Forms.TextBox txtComments;
     }
 }
--- InfirmerieGUI/GestionEleve.cs
+++ InfirmerieGUI/GestionEleve.cs
@@ -19,6 +19,15 @@ namespace InfirmerieGUI
         DateTimePicker dtp = new DateTimePicker();
         Rectangle _Rectangle;
         DataGridView dgv = new DataGridView();
+        private int idModif;
+        private string nomModif;
+        private string prenomModif;
+        private DateTime selectedDate;
+        private string NumEleveModif;
+        private string NumParentModif;
+        private string comments;
+        private bool check;
+        private bool extraTime;
 
         public GestionEleve()
         {
@@ -94,43 +103,43 @@ namespace InfirmerieGUI
             dataGridView1.DataSource = GestionInfirmerieBL.ToutLesEleves();
         }
 
-        private void AjouterEleve()
-        {
-            // Collecte des informations à partir des champs de saisie
-            string nom = textBoxNom.Text;
-            string prenom = textBoxPrenom.Text;
-            DateTime dateNaissance = dateTimePickerNaissance.Value;
-            string telEleve = textBoxTelEleve.Text;
-            // Assurez-vous d'avoir un moyen d'obtenir l'ID de la classe
-            int idClasse = int.Parse(comboBoxClasse.SelectedValue.ToString());
-            string telParent = textBoxTelParent.Text;
-            bool tiersTemps = checkBoxTiersTemps.Checked;
-            string commentaire = textBoxCommentaire.Text;
+        //private void AjouterEleve()
+        //{
+        //    //// Collecte des informations à partir des champs de saisie
+        //    //string nom = textBoxNom.Text;
+        //    //string prenom = textBoxPrenom.Text;
+        //    //DateTime dateNaissance = dateTimePickerNaissance.Value;
+        //    //string telEleve = textBoxTelEleve.Text;
+        //    //// Assurez-vous d'avoir un moyen d'obtenir l'ID de la classe
+        //    //int idClasse = int.Parse(comboBoxClasse.SelectedValue.ToString());
+        //    //string telParent = textBoxTelParent.Text;
+        //    //bool tiersTemps = checkBoxTiersTemps.Checked;
+        //    //string commentaire = textBoxCommentaire.Text;
 
-            // Création de l'objet Eleve
-            Eleve nouvelEleve = new Eleve(string nom, string prenom, );
-            {
-                Lastname = nom,
-                Firstname = prenom,
-                Birthdate = dateNaissance,
-                Phone = telEleve,
-                ClassNumber = new Classe(idClasse, /* Nom de la classe */),
-                ParentsPhone = telParent,
-                ExtraTime = tiersTemps,
-                Comment = commentaire
-            };
+        //    //// Création de l'objet Eleve
+        //    //Eleve nouvelEleve = new Eleve(string nom, string prenom, );
+        //    //{
+        //    //    Lastname = nom,
+        //    //    Firstname = prenom,
+        //    //    Birthdate = dateNaissance,
+        //    //    Phone = telEleve,
+        //    //    ClassNumber = new Classe(idClasse, /* Nom de la classe */),
+        //    //    ParentsPhone = telParent,
+        //    //    ExtraTime = tiersTemps,
+        //    //    Comment = commentaire
+        //    //};
 
-            // Ajout de l'élève via la BLL
-            GestionInfirmerieBL.GetGestionInfirmeries().AjouterEleve(nouvelEleve);
+        //    // Ajout de l'élève via la BLL
+        //    GestionInfirmerieBL.GetGestionInfirmeries().AjouterEleve(nouvelEleve);
 
-            // Mise à jour du DataGridView
-            ActualiserDataGridView();
-        }
+        //    // Mise à jour du DataGridView
+        //    ActualiserDataGridView();
+        //}
 
-        private void buttonAjouter_Click(object sender, EventArgs e)
-        {
-            AjouterEleve();
-        }
+        //private void buttonAjouter_Click(object sender, EventArgs e)
+        //{
+        //    AjouterEleve();
+        //}
 
 
         private void lblAccueil_Click(object sender, EventArgs e)
@@ -140,7 +149,28 @@ namespace InfirmerieGUI
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-          
+            pnlModif.Visible = true;
+
+
+            int.TryParse(txtIdModif.Text = dataGridView1.SelectedCells[0].Value.ToString(), out idModif);
+
+            nomModif = txtNomModif.Text = dataGridView1.SelectedCells[1].Value.ToString();
+            prenomModif = txtPrenomModif.Text = dataGridView1.SelectedCells[2].Value.ToString();
+
+            //Selection de la date a l'apparition du calendrier
+            DateTime date = new DateTime();
+            DateTime.TryParse(dataGridView1.SelectedCells[3].Value.ToString(), out date);
+            selectedDate = monthCalendar1.SelectionStart = date;
+
+             NumEleveModif = txtNumeroEleve.Text = dataGridView1.SelectedCells[4].Value.ToString();
+             NumParentModif = txtNumeroParent.Text = dataGridView1.SelectedCells[6].Value.ToString();
+
+            bool.TryParse(dataGridView1.SelectedCells[7].Value.ToString(), out check);
+            extraTime = chkExtraTime.Checked = check;
+            
+            comments = txtComments.Text = dataGridView1.SelectedCells[8].Value.ToString();
+
+
         }
 
         private void GestionEleve_Load(object sender, EventArgs e)
@@ -155,7 +185,10 @@ namespace InfirmerieGUI
 
         private void button2_Click(object sender, EventArgs e)
         {
-           
+            Classe uneClasse = new Classe(1, "Seconde1");
+            Eleve updateEleve = new Eleve(idModif, txtNomModif.Text, txtPrenomModif.Text, monthCalendar1.SelectionStart, txtNumeroEleve.Text, uneClasse, txtNumeroParent.Text, chkExtraTime.Checked, txtComments.Text);
+            EleveDAO.UpdateEleve(updateEleve);
+            ActualiserDataGridView();
         }
 
         private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
@@ -186,6 +219,40 @@ namespace InfirmerieGUI
         private void button1_Click_1(object sender, EventArgs e)
         {
             SupprimerEleve();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void txtBirthdateModif_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtComments_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
