79c308b3ab871d609ee9aded1abd7e66e72b4476
Sallak Imrane <imraneeslk@gmail.com>
2023-11-23 06:16:09 +0100

    Ajout DAO Medic

---
+++ InfirmerieBO/Medicament.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfirmerieBO
+{
+    class Medicament
+    {
+        private int id;
+        private string nom;
+
+        public Medicament(int id, string nom)
+        {
+            this.id = id;
+            this.nom = nom;
+        }
+
+    }
+}
