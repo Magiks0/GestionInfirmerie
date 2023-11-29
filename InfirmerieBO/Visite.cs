cb396d55f3b7ebccba332b6fb946ec11aac00642
Lucas DUPAS <dupaslucas8@gmail.com>
2023-11-29 11:20:58 +0100

    Création TOUT de la Classe Visite -> BO Création Classe VisiteDAO ->  DAL

---
+++ InfirmerieBO/Visite.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfirmerieBO
+{
+    public class Visite
+    {
+        #region Déclaration des attributs privé
+        private int id;
+        private Eleve idEleve;
+        private DateTime heureDebutVisite;
+        private DateTime heureFinVisite;
+        private string motifVisite;
+        private string commentaireVisite;
+        private bool renvoiDomicile;
+        private bool hospitalisation;
+        private bool parentsPrevenus;
+        private Medicament idMedicament;
+        private string quantiteMeidcament;
+
+        #endregion
+
+        #region Constructeurs
+        //Constructeur Visite avec ID
+        public Visite(int id, Eleve idEleve, DateTime heureDebutVisite, DateTime heureFinVisite, string motifVisite, string commentaireVisite, bool renvoiDomicile, bool hospitalisation, bool parentsPrevenus, Medicament idMedicament, string quantiteMeidcament)
+        {
+            this.id = id;
+            this.idEleve = idEleve;
+            this.heureDebutVisite = heureDebutVisite;
+            this.heureFinVisite = heureFinVisite;
+            this.motifVisite = motifVisite;
+            this.commentaireVisite = commentaireVisite;
+            this.renvoiDomicile = renvoiDomicile;
+            this.hospitalisation = hospitalisation;
+            this.parentsPrevenus = parentsPrevenus;
+            this.idMedicament = idMedicament;
+            this.quantiteMeidcament = quantiteMeidcament;
+        }
+
+        //Constructeur Visite sans ID
+        public Visite(Eleve idEleve, DateTime heureDebutVisite, DateTime heureFinVisite, string motifVisite, string commentaireVisite, bool renvoiDomicile, bool hospitalisation, bool parentsPrevenus, Medicament idMedicament, string quantiteMeidcament)
+        {
+            this.idEleve = idEleve;
+            this.heureDebutVisite = heureDebutVisite;
+            this.heureFinVisite = heureFinVisite;
+            this.motifVisite = motifVisite;
+            this.commentaireVisite = commentaireVisite;
+            this.renvoiDomicile = renvoiDomicile;
+            this.hospitalisation = hospitalisation;
+            this.parentsPrevenus = parentsPrevenus;
+            this.idMedicament = idMedicament;
+            this.quantiteMeidcament = quantiteMeidcament;
+        }
+        #endregion
+
+        #region Getters/Setters
+        public int Id { get => id; set => id = value; }
+        public Eleve IdEleve { get => idEleve; set => idEleve = value; }
+        public DateTime HeureDebutVisite { get => heureDebutVisite; set => heureDebutVisite = value; }
+        public DateTime HeureFinVisite { get => heureFinVisite; set => heureFinVisite = value; }
+        public string MotifVisite { get => motifVisite; set => motifVisite = value; }
+        public string CommentaireVisite { get => commentaireVisite; set => commentaireVisite = value; }
+        public bool RenvoiDomicile { get => renvoiDomicile; set => renvoiDomicile = value; }
+        public bool Hospitalisation { get => hospitalisation; set => hospitalisation = value; }
+        public bool ParentsPrevenus { get => parentsPrevenus; set => parentsPrevenus = value; }
+        public Medicament IdMedicament { get => idMedicament; set => idMedicament = value; }
+        public string QuantiteMeidcament { get => quantiteMeidcament; set => quantiteMeidcament = value; }
+        #endregion
+    }
+}
